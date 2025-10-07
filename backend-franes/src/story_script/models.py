6eded1284c0c646825175ed848d5ae5c74860e19
Messyas <messyasgoisfranca@gmail.com>
2025-10-06 20:40:14 -0400

    strory script model

---
+++ backend-franes/src/story_script/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    func,
+    Table,
+)
+from src.database import metadata
+
+story_script = Table(
+    "story_script",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(60), nullable=False),
+    Column("sub_title", String(120), nullable=False),
+    Column("created_at", DateTime, server_default=func.now(), nullable=False),
+    Column("author_notes", String(300), nullable=True),
+    Column("content", String, nullable=False),
+    Column("author_final_comment", String, nullable=True)
+)
